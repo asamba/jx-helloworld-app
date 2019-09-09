1ee54ecbdf18db1e567aebd2f20ef66b39a936f1
asamba <anand.sambamoorthy@gmail.com>
2019-09-09 13:38:54 -0700

    hello controller

---
+++ src/main/java/my/projects/jx/jxhelloworldapp/HelloController.java
@@ -0,0 +1,19 @@
+package my.projects.jx.jxhelloworldapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by asamba on 9/9/19.
+ */
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello World - from RestFul Service";
+    }
+
+}
